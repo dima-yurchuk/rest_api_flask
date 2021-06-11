83df81e59f3874bede8bfaccd30247b0e520978f
dima-yurchuk <dimayurchuk20@gmail.com>
2021-06-11 12:13:43 +0300

    Heroku

--- commands.py
+++ commands.py
@@ -1,8 +1,7 @@
 import click
 from flask.cli import with_appcontext
 from app import db
-from app.profile.models import User
-from app.task.models import Task, Category, Employee
+from app.api_restfull.models import Subject
 
 @click.group()
 def cli():
@@ -15,21 +14,7 @@ def create_tables():
     click.echo("tables created!!!")
 
 
-@click.command(name='create_admin')
-@with_appcontext
-def create_admin():
-    username = "test_admin"
-    email = "test_admin@gmail.com"
-    password = "11111111"
-    admin = True
-    user = User(username=username, email=email, password=password, admin=admin)
-    #print("superadmin created", u)
-    db.session.add(user)
-    db.session.commit()
-    click.echo("admin created!!!")
-
 cli.add_command(create_tables)
-cli.add_command(create_admin)
 
 
 if __name__ == '__main__':
